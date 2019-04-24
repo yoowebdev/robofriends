5c16440ff1b996e86067cfa304abf6a738f55ec4
yoowebdevlife <yoowebdev@gmail.com>
2019-04-24 16:29:30 -0500

    Scroll Component and build files

---
+++ build/precache-manifest.678731eaf59508681a9f26f96252f884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "65aec7d02c8591a4a60c",
+    "url": "/static/js/main.9530f3b5.chunk.js"
+  },
+  {
+    "revision": "b1a7addda6c36203bfb8",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "65aec7d02c8591a4a60c",
+    "url": "/static/css/main.d36bf56f.chunk.css"
+  },
+  {
+    "revision": "b1a7addda6c36203bfb8",
+    "url": "/static/css/2.7f58b14a.chunk.css"
+  },
+  {
+    "revision": "4f58b5353a62504de5b04286c5e7ca9a",
+    "url": "/index.html"
+  }
+];
