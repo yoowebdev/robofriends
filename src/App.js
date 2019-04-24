b02998db4aca4f31d990e0b78a82b432071ee6fa
yoowebdevlife <yoowebdev@gmail.com>
2019-04-24 15:17:35 -0500

    added state and CSS styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 import { robots } from "./robots";
-
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -14,17 +14,18 @@ class App extends Component {
   }
 
   onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value})
+    this.setState({ searchfield: event.target.value});
   }
 
   render() {
-      const filteredRobots = this.state.robots.filter(robots => {
-          return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-      })
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    console.log(filteredRobots);
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
     );
